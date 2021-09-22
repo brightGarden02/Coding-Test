b7ff55d9b228a7c5c8fe03ad0683117ec377a7ca
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-09-23 00:27:46 +0900

    Add files via upload

--- level2/13_카펫.java
+++ level2/13_카펫.java
@@ -4,7 +4,18 @@ class Solution {
         
         int area = brown + yellow;
         
-        
+        for(int col = 3; col <= area; col++){
+            int row = area/col;
+            
+            if(row < col){
+                continue;
+            }
+            if((row-2)*(col-2) == yellow){
+                answer[0] = row;
+                answer[1] = col;
+                break;
+            }
+        }
         
         return answer;
     }
