730e0a0123dff54874004a50a35031ff0e3c5b41
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-09-23 00:16:00 +0900

    Add files via upload

---
+++ level2/13_카펫.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int area = brown + yellow;
+        
+        
+        
+        return answer;
+    }
+}
