01442c73437b9d7b8e2767430ac293e346465c5f
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-09-27 23:52:13 +0900

    Add files via upload

---
+++ 1427_소트인사이드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		char[] answerArray = in.nextLine().toCharArray();
+ 
+ 
+		Arrays.sortanswerArray 
+ 
+		for (int i = answerArray - 1; i >= 0; i--) {
+			System.out.printanswerArray [i]);
+		}
+ 
+	}
+}
