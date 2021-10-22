cec9bfb4fe0692d63163af6abb7b4e7d5c928f43
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-10-23 00:11:52 +0900

    Add files via upload

---
+++ leetcode/easy/Array/2037_Minimum Number of Moves to Seat Everyone.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        
+        int answer = 0;
+        for(int i = 0; i < seats.length; i++){
+            answer += Math.abs(seats[i] - students[i]);
+            
+        }
+        
+        return answer;
+    }
+}
