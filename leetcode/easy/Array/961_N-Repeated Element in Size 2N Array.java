27a1016de8dca799b63e27a5d4fe4a4d08c9a1f9
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-11-23 20:30:09 +0900

    Add files via upload

---
+++ leetcode/easy/Array/961_N-Repeated Element in Size 2N Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i]))
+                return nums[i];
+            set.add(nums[i]);
+        }
+        
+        return -1;
+    }
+}
