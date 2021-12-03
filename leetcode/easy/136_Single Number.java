6b4b3c7ed42dbc45253c275d04a948db461eb9ec
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-12-03 14:06:50 +0900

    Add files via upload

---
+++ leetcode/easy/136_Single Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int answer = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            answer = answer ^ nums[i];
+        }
+        return answer;
+    }
+}
