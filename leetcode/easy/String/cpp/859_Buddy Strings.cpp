f56f902e4d647bdaf50d24477616b6eec72af1a0
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-12-29 19:50:09 +0900

    Add files via upload

---
+++ leetcode/easy/String/cpp/859_Buddy Strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        
+         if(s.length() != goal.length()) return false;
+        
+        if(s == goal)
+        {
+            unordered_set<char> a = unordered_set<char>(s.begin(), s.end());  
+            
+            return a.size() < s.length();
+        }
+        
+        vector<int> v;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] != goal[i]) v.push_back(i);
+        }
+        
+        return (v.size() == 2 && s[v[0]] == goal[v[1]] && s[v[1]] == goal[v[0]]); 
+    }
+};
