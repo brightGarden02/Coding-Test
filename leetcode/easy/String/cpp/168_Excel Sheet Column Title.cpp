e68ef7bfacee382df217f6954296e4a31dd8ae9b
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2021-12-30 22:54:36 +0900

    Add files via upload

---
+++ leetcode/easy/String/cpp/168_Excel Sheet Column Title.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        
+        string ans;
+        while(columnNumber != 0)
+        {  
+            ans += ((columnNumber - 1) % 26) +'A';
+            columnNumber = (columnNumber - 1) / 26;
+        }
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
