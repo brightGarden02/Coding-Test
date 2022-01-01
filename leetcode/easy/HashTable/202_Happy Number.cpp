34ed0f9feec222aab2a364a233b318d802b66623
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-01 19:10:58 +0900

    Add files via upload

---
+++ leetcode/easy/HashTable/202_Happy Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        unordered_map<int, int> map;
+        int res;
+        
+        while(n != 1){
+		
+			//check cycle
+            if(map[n]) return false;
+            map[n]++;
+            
+            res = 0;
+			while(n){
+                res += pow(n%10, 2);
+                n /= 10;
+            }
+            n = res;
+        }
+        
+        return true;
+    }
+};
