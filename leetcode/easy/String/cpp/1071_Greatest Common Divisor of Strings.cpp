a3db47ba81108732433fda25a8a3bb217ee1b966
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-03 08:50:31 +0900

    Update 1071_Greatest Common Divisor of Strings.cpp

--- leetcode/easy/String/cpp/1071_Greatest Common Divisor of Strings.cpp
+++ leetcode/easy/String/cpp/1071_Greatest Common Divisor of Strings.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
         
-        return str1 + str2 == str2 + str1 ? str1.substr(0, my_gcd(str1.size(), str2.size())) : "";
+        return str1 + str2 == str2 + str1
+            ? str1.substr(0, my_gcd(str1.size(), str2.size())) : "";
     }
     
-    private:
-        int my_gcd(int a, int b){
+    int my_gcd(int a, int b){
             
-            if(b == 0) return a;
-            return my_gcd(b, a % b);
+        if(b == 0) return a;
+        return my_gcd(b, a % b);
             
-        }
-    
+    }
 };
