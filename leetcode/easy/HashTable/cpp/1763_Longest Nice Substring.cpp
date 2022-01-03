214bca119c3e0e8b94f057dc851aaa5598aa7799
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-04 08:44:51 +0900

    Update 1763_Longest Nice Substring.cpp

--- leetcode/easy/HashTable/cpp/1763_Longest Nice Substring.cpp
+++ leetcode/easy/HashTable/cpp/1763_Longest Nice Substring.cpp
@@ -10,15 +10,12 @@ public:
             char upper = toupper(s[i]);
             char lower = tolower(s[i]);
             
-            if(st.find(upper) == end(st)
-               || st.find(lower) == end(st)){
+            if(st.find(upper) == end(st) || st.find(lower) == end(st)){
             
                 string s1 = longestNiceSubstring(s.substr(0, i));
                 string s2 = longestNiceSubstring(s.substr(i+1));
                 
-                
                 return s1.size() >= s2.size() ? s1 : s2;
-                
             }
         }
         
