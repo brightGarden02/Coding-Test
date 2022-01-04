0daaabfb73f2a1d0ab7991604ecbb03a39140a89
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-05 08:27:38 +0900

    Update 1688_Count of Matches in Tournament.cpp

--- leetcode/easy/Math/cpp/1688_Count of Matches in Tournament.cpp
+++ leetcode/easy/Math/cpp/1688_Count of Matches in Tournament.cpp
@@ -3,17 +3,17 @@ public:
     int numberOfMatches(int n) {
         
         int ans = 0;
-        int teamAdvance = n;
+        int teamAdv = n;
         int matches = 0;
-        while(teamAdvance != 1){
+        while(teamAdv != 1){
             
-            if(teamAdvance%2 == 0){
-                teamAdvance = teamAdvance/2;
+            if(teamAdv%2 == 0){
+                teamAdv = teamAdv/2;
             }
             else{
-                teamAdvance = teamAdvance - teamAdvance/2;
+                teamAdv = teamAdv - teamAdv/2;
             }
-            matches = n - teamAdvance;
+            matches = n - teamAdv;
             n = n - matches;
             ans = ans + matches;
         }
