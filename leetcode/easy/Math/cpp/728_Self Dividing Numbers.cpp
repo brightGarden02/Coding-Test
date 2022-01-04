678b32dc45f3951cf7fbed0f24c0c0be181c1df9
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-04 13:45:05 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/728_Self Dividing Numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        
+        vector<int> v;
+        for(int i = left; i <= right; i++){
+            
+            string tmpStr = to_string(i);
+            int divZero = 0;
+            int tmp = i;
+            int j = 0;
+            while(j < tmpStr.size()){
+                
+                int d = tmp % 10;
+                if(d != 0 && (i % d == 0)){
+                    divZero++;
+                }
+                tmp = tmp/10;
+                j++;
+                
+            }
+            if(divZero == tmpStr.size()){
+                v.push_back(i);
+            }
+        }
+        
+        return v;
+    }
+};
