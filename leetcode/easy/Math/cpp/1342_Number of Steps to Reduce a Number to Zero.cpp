5dd7f63f39a4ca81c51fb096566dc7a8f5f2aae9
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-04 12:13:33 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/1342_Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        
+        int ans = 0;
+        while(num){
+            
+            if(num%2 == 0){
+                num = num/2;
+            }
+            else{
+                num = num - 1;
+            }
+            
+            ans++;
+        }
+        
+        return ans;
+    }
+};
