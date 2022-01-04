24d650c2bebe91f95751e7752224331ac721a272
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-05 08:22:59 +0900

    Update 1342_Number of Steps to Reduce a Number to Zero.cpp

--- leetcode/easy/Math/cpp/1342_Number of Steps to Reduce a Number to Zero.cpp
+++ leetcode/easy/Math/cpp/1342_Number of Steps to Reduce a Number to Zero.cpp
@@ -5,12 +5,7 @@ public:
         int ans = 0;
         while(num){
             
-            if(num%2 == 0){
-                num = num/2;
-            }
-            else{
-                num = num - 1;
-            }
+            (num%2 == 0) ? (num = num/2) : (num = num-1);
             
             ans++;
         }
