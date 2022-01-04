69aafddff70fc39a3e3dd64182c6cd9579f3a3c5
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-04 13:22:17 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/2119_A Number After a Double Reversal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isSameAfterReversals(int num) {
+        
+        if(num == 0) return true;
+        string numStr = to_string(num);
+        
+        string str1 = reverseNumber(num);
+        
+        num = stoi(str1);
+        string str2 = reverseNumber(num);
+        
+        return numStr == str2;
+    }
+    
+    string reverseNumber(int num){
+        
+        string str = "";
+        while(num){
+            
+            int res = num % 10;
+            str += to_string(res);
+            num = num / 10;
+            
+        }
+        return str;
+    }
+};
