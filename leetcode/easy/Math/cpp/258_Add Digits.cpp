baaf1e362655ad2289fb778ffb939dd8e506c729
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-05 19:32:22 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/258_Add Digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        int ans = 0;
+        if(to_string(num).size() == 1) return num;
+        
+        while(num){
+            
+            int rem = num % 10;
+            
+            ans += rem;
+            num = num / 10;
+        }
+        
+        num = ans;
+        
+        return addDigits(num);
+    }
+};
