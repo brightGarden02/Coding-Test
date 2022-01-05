c7f2f8978a1a71e9c538f3882bfeee78d330d989
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-05 14:26:55 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/1716_Calculate Money in Leetcode Bank.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        
+        int ans = 0;
+        int start = 1;
+        int j = 1;
+        for(int i = 1; i <= n; i++){
+            
+            ans = ans + start;
+            start++;
+            
+            if(i%7 == 0){
+                j++;
+                start = j;
+            }
+            
+        }
+        return ans;
+    }
+};
