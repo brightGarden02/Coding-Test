a2516dce5d6a6fb8c3bd77de41df51dc6146b82c
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-05 15:02:36 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/1103_Distribute Candies to People.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int num_people) {
+        
+        vector<int> ans(num_people);
+        
+        int i = 1;
+        int p = 0;
+        
+        while(candies > 0){
+            
+            if(i > candies) ans[p] += candies;
+            else ans[p] += i;
+            
+            candies = candies - i;
+            
+            p++;
+            if(p == num_people){
+                p = 0;   
+            }
+            i++;
+        }
+        
+        return ans;
+    }
+};
