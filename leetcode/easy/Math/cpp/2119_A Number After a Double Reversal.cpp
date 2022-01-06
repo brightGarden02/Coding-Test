0ddb1bafdbd713137256ca95897ea9fa903fb067
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-06 11:07:19 +0900

    Update 2119_A Number After a Double Reversal.cpp

--- leetcode/easy/Math/cpp/2119_A Number After a Double Reversal.cpp
+++ leetcode/easy/Math/cpp/2119_A Number After a Double Reversal.cpp
@@ -6,7 +6,6 @@ public:
         string numStr = to_string(num);
         
         string str1 = reverseNumber(num);
-        
         num = stoi(str1);
         string str2 = reverseNumber(num);
         
@@ -17,11 +16,9 @@ public:
         
         string str = "";
         while(num){
-            
             int res = num % 10;
             str += to_string(res);
             num = num / 10;
-            
         }
         return str;
     }
