767a9c34c1a4eb3d364f8088e692cd0519eaf058
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-07 14:20:02 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/590_N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        
+        vector<int> post;
+        
+        traverse(root, post);
+        return post;
+    }
+    
+    void traverse(Node* root, vector<int> &post){
+        
+        if(root == NULL) return;
+        for(auto x : root->children){
+            traverse(x, post);
+        }
+        post.push_back(root->val);
+    }
+};
