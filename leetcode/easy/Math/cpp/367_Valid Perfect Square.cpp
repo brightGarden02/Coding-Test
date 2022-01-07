5e6ff4723699c857924ea27b46c5864d9d5e15e6
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-07 13:37:48 +0900

    Add files via upload

---
+++ leetcode/easy/Math/cpp/367_Valid Perfect Square.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+        long long i = 1;
+        while(i*i <= num){
+            
+            if(i*i == num) return true;
+            i++;
+        }
+        
+        return false;
+    }
+};
