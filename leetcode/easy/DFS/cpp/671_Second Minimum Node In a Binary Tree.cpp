2d1072b48811437dded9eade24d06edde0735c5e
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-11 19:14:10 +0900

    Add files via upload

---
+++ leetcode/easy/DFS/cpp/671_Second Minimum Node In a Binary Tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        
+        vector<int> v;
+        
+        traverse(root, v);
+        int minValue = INT_MAX;
+        int minSecondValue = INT_MAX;
+        for(int i = 0; i < v.size(); i++){
+            minValue = min(minValue, v[i]);
+        }
+        
+        bool check = false;
+        for(int i = 0; i < v.size(); i++){
+            
+            if(v[i] <= minSecondValue && v[i] != minValue){
+                minSecondValue = min(minSecondValue, v[i]);
+                check = true;
+            }
+        }
+        
+        if(check == false) return -1;
+        
+        return minSecondValue;
+    }
+    
+    void traverse(TreeNode* root, vector<int>& v){
+        
+        if(root == NULL) return;
+        traverse(root->left, v);
+        v.push_back(root->val);
+        traverse(root->right, v);
+    }
+};
