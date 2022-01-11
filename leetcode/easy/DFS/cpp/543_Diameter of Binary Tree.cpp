9f300a6a0e322ddbc7903bf9e73b04cc49357b4b
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-11 18:43:24 +0900

    Add files via upload

---
+++ leetcode/easy/DFS/cpp/543_Diameter of Binary Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameter = 0;
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        moveDepth(root);
+        return diameter;
+    }
+    
+    int moveDepth(TreeNode* root){
+        if(!root)   return 0;
+        int l = moveDepth(root->left);
+        int r = moveDepth(root->right);
+        
+        diameter = max(diameter, l+r);
+        
+        return 1 + max(l, r);
+    }
+};
