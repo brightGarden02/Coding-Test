bbc3db06c20c4676edc1d96f1f9239111a3d6c97
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-11 23:22:09 +0900

    Add files via upload

---
+++ leetcode/easy/DFS/cpp/1971_Find if Path Exists in Graph.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> graph;
+    vector<bool> visited;
+    bool validPath(int n, vector<vector<int>>& edges, int start, int end){
+        
+        graph.resize(n);
+        visited.resize(n);
+        for(auto &edge : edges){
+            
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        
+        return dfs(start, end);
+    }
+    
+    bool dfs(int curr, int end){
+        
+        if (curr == end) return true;
+        if (visited[curr]) return false;
+        
+        visited[curr] = true;
+        for(int next : graph[curr]){
+            
+            if(dfs(next, end)) return true;
+        } 
+        
+        return false;
+    }
+};
