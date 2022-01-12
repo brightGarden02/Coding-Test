eaf50fa1a51fad00cff9be3064ec3b58b7bbbaba
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-12 14:45:45 +0900

    Add files via upload

---
+++ leetcode/easy/DFS/cpp/1984_Minimum Difference Between Highest and Lowest of K Scores.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        if(n == 1) return 0;
+        
+        sort(nums.begin(), nums.end());
+        int score = INT_MAX;
+        
+        for(int i = 0; i <= n-k; i++){
+            
+            score = min(score, nums[k+i-1]-nums[i]);
+        }
+        
+        return score;
+    }
+};
