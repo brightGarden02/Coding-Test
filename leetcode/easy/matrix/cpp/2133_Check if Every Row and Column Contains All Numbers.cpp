9562c0d2acac929286468657d00bdb5fba4d7b3c
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-12 16:19:08 +0900

    Add files via upload

---
+++ leetcode/easy/matrix/cpp/2133_Check if Every Row and Column Contains All Numbers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        
+       int n = matrix.size();
+        
+       set<int> rows;
+       for(int i = 0; i < n; i++)
+       {
+           for(int j = 0; j < n; j++)
+           {
+               rows.insert(matrix[i][j]);
+           }
+           if(rows.size() != n) return false;
+           rows.clear();
+       }
+       
+       set<int> cols;
+       for(int i = 0; i < n; i++)
+       {
+           for(int j = 0; j < n;j++)
+           {
+               cols.insert(matrix[j][i]);
+           }
+           if(cols.size() != n) return false;
+           cols.clear();
+       }
+        
+       return true;
+    }
+};
