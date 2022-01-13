3f154873f18399e633ea3f4ce659fe08cc3efb7b
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-13 11:56:38 +0900

    Add files via upload

---
+++ leetcode/easy/Bit Manipulation/cpp/1009_Complement of Base 10 Integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int bitwiseComplement(int num) {
+        
+        if(num == 0) return 1;
+        vector<int> temp; 
+        
+		while(num != 0){
+            temp.push_back(num % 2);
+            num /= 2;
+        }
+        
+        for(int i = 0; i < temp.size(); i++){
+            if(temp[i] == 1) temp[i] = 0;
+            else if(temp[i] == 0) temp[i] = 1;
+        }
+        
+        int res = 0;
+		for(int i = temp.size()-1; i >= 0; i--){
+            res = res * 2 + temp[i];
+        }
+        
+        return res;
+    }
+};
