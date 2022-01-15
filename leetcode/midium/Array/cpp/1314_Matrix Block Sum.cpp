0fdd1798c5a7cd302a8501518169663cc97bee2a
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-15 11:19:32 +0900

    Add files via upload

---
+++ leetcode/midium/Array/cpp/1314_Matrix Block Sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+        
+        int m = mat.size(), n = mat[0].size();
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                
+                if(i > 0) mat[i][j] += mat[i-1][j];
+                if(j > 0) mat[i][j] += mat[i][j-1];
+                if(i > 0 && j > 0) mat[i][j] -= mat[i-1][j-1];
+            }
+        }
+        vector<vector<int>> ans(m, vector<int>(n));
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                
+                int r0 = max(0, i-k), c0 = max(0, j-k);
+                int r1 = min(m-1, i+k), c1 = min(n-1, j+k);
+                
+                ans[i][j] = mat[r1][c1];
+                if(r0 > 0) ans[i][j] -= mat[r0-1][c1];
+                if(c0 > 0) ans[i][j] -= mat[r1][c0-1];
+                if(r0 > 0 && c0 > 0) ans[i][j] += mat[r0-1][c0-1];
+            }
+        }
+        
+        return ans;
+    }
+};
