3af80727caaab5bd13b0ae607a48c5f5cb1f0f4d
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-17 23:36:48 +0900

    Update 1605_Find Valid Matrix Given Row and Column Sums.cpp

--- leetcode/midium/Array/cpp/1605_Find Valid Matrix Given Row and Column Sums.cpp
+++ leetcode/midium/Array/cpp/1605_Find Valid Matrix Given Row and Column Sums.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
         
         int m = rowSum.size();
-		int n = colSum.size();
+	int n = colSum.size();
         vector<vector<int>> mat(m, vector<int>(n, 0));
         for (int i = 0; i < m; i++) {
             for (int j = 0 ; j < n; j++) {
