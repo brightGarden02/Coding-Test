710eadf9015e9cc1e7c11f5e53e582767042ba99
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-18 12:16:51 +0900

    Update 739_Daily Temperatures.cpp

--- leetcode/midium/Array/cpp/739_Daily Temperatures.cpp
+++ leetcode/midium/Array/cpp/739_Daily Temperatures.cpp
@@ -3,16 +3,17 @@ public:
     vector<int> dailyTemperatures(vector<int>& t) {
         
         int n = t.size();
-	    stack<int> s;
-	    vector<int> ans(n, 0);
-	    for (int i = 0; i < n; i++) {
-		    while (!s.empty() && t[s.top()] < t[i]) {
-                int j = s.top();
-                s.pop();
-                ans[j] = i - j;
-		    }
-		    s.push(i);
-	    }
-	    return ans;
+	stack<int> s;
+	vector<int> ans(n, 0);
+	for(int i = 0; i < n; i++) {
+		while(!s.empty() && t[s.top()] < t[i]) {
+                	int j = s.top();
+                	s.pop();
+                	ans[j] = i - j;
+		}
+		s.push(i);
+	}
+	    
+	return ans;
     }
 };
