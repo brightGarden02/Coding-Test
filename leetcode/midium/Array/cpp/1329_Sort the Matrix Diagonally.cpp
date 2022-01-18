5459759c9fe7590436dbe180f85d4f4f69b8ef0b
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-18 11:49:47 +0900

    Update 1329_Sort the Matrix Diagonally.cpp

--- leetcode/midium/Array/cpp/1329_Sort the Matrix Diagonally.cpp
+++ leetcode/midium/Array/cpp/1329_Sort the Matrix Diagonally.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& matrix) {
         
-        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> diagonal;
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>> > diagonal;
         int m = matrix.size(), n = matrix[0].size();
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
