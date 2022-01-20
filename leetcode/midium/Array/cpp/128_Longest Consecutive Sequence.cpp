039892202e29327e88b3363b894fd1abe249b844
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-20 20:14:46 +0900

    Update 128_Longest Consecutive Sequence.cpp

--- leetcode/midium/Array/cpp/128_Longest Consecutive Sequence.cpp
+++ leetcode/midium/Array/cpp/128_Longest Consecutive Sequence.cpp
@@ -7,10 +7,10 @@ public:
 	int longest = 0, cur_longest = 1;
         
 	for(int i = 1; i < nums.size(); i++){
-            if(nums[i] == nums[i-1]){
+            if(nums[i-1] == nums[i]){
                 continue;    
             } 
-	    else if(nums[i] == nums[i-1] + 1){
+	    else if(nums[i-1]+1 == nums[i]){
                 cur_longest++;    
             } 
             else{
