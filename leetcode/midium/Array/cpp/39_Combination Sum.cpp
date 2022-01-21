72362e3e47fed4fa7d5a720438aef8c66371cc5c
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-21 18:30:03 +0900

    Update 39_Combination Sum.cpp

--- leetcode/midium/Array/cpp/39_Combination Sum.cpp
+++ leetcode/midium/Array/cpp/39_Combination Sum.cpp
@@ -3,31 +3,27 @@ public:
     vector<vector<int>> ans;
     vector<int> cur;
     int sum;
-
+    vector<vector<int>> combinationSum(vector<int>& candidates, int t) {
+            
+            sum = 0; 
+            function(candidates, t, 0);
+            return ans;
+    }
+    
     void function(vector<int>& candidates, int t, int index){
     
         if(sum > t) return;
-
-        if(sum == t){
-            ans.push_back(cur);
-        }
+        if(sum == t) ans.push_back(cur);
         
         for(int i = index; i < candidates.size(); i++){
-
-            cur.push_back(candidates[i]);
+            
             sum += candidates[i];
+            cur.push_back(candidates[i]);
             
             function(candidates, t, i);
             
             sum -= candidates[i];
-            cur.pop_back();  
+            cur.pop_back();
         }      
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int t) {
-            sum = 0; 
-            function(candidates, t, 0);
-
-            return ans;
     }
 };
