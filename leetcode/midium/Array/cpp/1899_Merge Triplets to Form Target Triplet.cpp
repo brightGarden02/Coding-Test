23b5e585863a13abd6c1a444805553fd2adfb988
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-22 09:36:33 +0900

    Update 1899_Merge Triplets to Form Target Triplet.cpp

--- leetcode/midium/Array/cpp/1899_Merge Triplets to Form Target Triplet.cpp
+++ leetcode/midium/Array/cpp/1899_Merge Triplets to Form Target Triplet.cpp
@@ -3,11 +3,11 @@ public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         
         vector<int> ans(3);
-        for (auto &t : triplets)
-        if(t[0] <= target[0] && t[1] <= target[1] && t[2] <= target[2]){
+        for (auto &t : triplets){
             
-            ans = { max(ans[0], t[0]), max(ans[1], t[1]), max(ans[2], t[2]) };   
-            
+            if(t[0] <= target[0] && t[1] <= target[1] && t[2] <= target[2]){
+                ans = { max(ans[0], t[0]), max(ans[1], t[1]), max(ans[2], t[2]) };   
+            }   
         }
             
         return ans == target;  
