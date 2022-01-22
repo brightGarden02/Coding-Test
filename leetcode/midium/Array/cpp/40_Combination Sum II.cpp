7f0eea235b188bf623a7978ef924ce855104f12e
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-22 12:13:10 +0900

    Add files via upload

---
+++ leetcode/midium/Array/cpp/40_Combination Sum II.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> cur;
+    int sum;
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        
+        sort(candidates.begin(), candidates.end());
+        combination(candidates, target, 0);
+        
+        return ans;
+    }
+    
+    void combination(vector<int>& candidates, int target, int starti){
+        if(sum > target) return;
+        if(sum == target){
+            ans.push_back(cur);
+        }
+            
+        for(int i = starti; i < candidates.size(); i++){
+            if(i != starti && candidates[i] == candidates[i-1])    
+                continue;
+            
+            sum += candidates[i];
+            cur.push_back(candidates[i]);
+            
+            combination(candidates, target, i+1);
+            
+            sum -= candidates[i];
+            cur.pop_back();
+        }
+    }
+};
