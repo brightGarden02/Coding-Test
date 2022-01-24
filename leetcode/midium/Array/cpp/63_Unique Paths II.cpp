640ea957eec0f34c3c9a1fc238ac486830c92f01
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-25 08:20:35 +0900

    Update 63_Unique Paths II.cpp

--- leetcode/midium/Array/cpp/63_Unique Paths II.cpp
+++ leetcode/midium/Array/cpp/63_Unique Paths II.cpp
@@ -14,12 +14,14 @@ public:
     int solve(vector<vector<int>>& grid, int i, int j) {
         
         if(i < 0 || j < 0 || i >= m || j >= n) return 0;    // exceed range
-    
         
-        if(grid[i][j]) return dp[i][j] = 0;                 // found obstacle
+        if(grid[i][j] != 0) return dp[i][j] = 0;            // if grid[i][j] != 0  found obstacle
         if(i == m - 1 && j == n - 1) return 1;              // finish
+       
+        // if dp[i][j] != 0 :
+        // if already computed for current cell, just return the stored results
+        if(dp[i][j] != 0) return dp[i][j];                      
         
-        if(dp[i][j]) return dp[i][j];                       // if already computed for current cell, just return the stored results
         
         return dp[i][j] = solve(grid, i+1, j) + solve(grid, i, j+1);
     }
