cf0e5d64d71acba22bbec33ee5b2defa8865e092
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-26 18:17:03 +0900

    Add files via upload

---
+++ leetcode/midium/Array/cpp/162_Find Peak Element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+        int l = 0, r = nums.size()-1, mid;
+        
+        while(l < r) {
+            
+            mid = l + (r - l) / 2;
+            
+            if (nums[mid] < nums[mid + 1]){
+                l = mid + 1;       
+            } 
+            else{
+                r = mid;    
+            } 
+        }
+        
+        return l;
+    }
+};
