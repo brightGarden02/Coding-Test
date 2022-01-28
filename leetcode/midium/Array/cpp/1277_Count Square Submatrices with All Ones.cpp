9bb4f5e96d69d7117bdf6b3866c297f883f2b4f8
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-01-29 08:47:47 +0900

    Update 1277_Count Square Submatrices with All Ones.cpp

--- leetcode/midium/Array/cpp/1277_Count Square Submatrices with All Ones.cpp
+++ leetcode/midium/Array/cpp/1277_Count Square Submatrices with All Ones.cpp
@@ -3,10 +3,10 @@ public:
     int countSquares(vector<vector<int>>& matrix) {
         
         int ans = 0;
-        int r = matrix.size();
-        int c = matrix[0].size();
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 
                 if(i > 0 && j > 0 && matrix[i][j] > 0){
                     matrix[i][j] = min(matrix[i-1][j-1], min(matrix[i-1][j], matrix[i][j-1])) + 1;
