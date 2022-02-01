20f8cc0332b50ec280b22df3a5e8d36312915a60
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-02-01 09:27:35 +0900

    Update 1442_Count Triplets That Can Form Two Arrays of Equal XOR.cpp

--- leetcode/midium/Array/cpp/1442_Count Triplets That Can Form Two Arrays of Equal XOR.cpp
+++ leetcode/midium/Array/cpp/1442_Count Triplets That Can Form Two Arrays of Equal XOR.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     int countTriplets(vector<int>& arr) {
         
-	int n = arr.size();
-	if(n == 0) return 0;
-	int two_value_arrays[n];
-	int count = 0;
+        int n = arr.size();
+        if(n == 0) return 0;
         
-	two_value_arrays[0] = arr[0];
-	for(int i = 1; i < n; i++){
-        	two_value_arrays[i] = two_value_arrays[i-1] ^ arr[i];
-    	}
-			
+        int two_value_arrays[n];
+        int count = 0;
         
-    	for(int i = 0; i < n; i++){
-		if(two_value_arrays[i] == 0) count += i;
-	}
+        two_value_arrays[0] = arr[0];
+        for(int i = 1; i < n; i++){
+            two_value_arrays[i] = two_value_arrays[i-1] ^ arr[i];
+        }
         
-	for(int i = 0; i < n; i++){
-		for(int j = i+1; j < n; j++){
-			if(two_value_arrays[i] == two_value_arrays[j])
-				count += j-(i+1);
-		}
-	}
+        for(int i = 0; i < n; i++){
+            if(two_value_arrays[i] == 0) count += i;
+        }
         
-	return count;
-	}
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(two_value_arrays[i] == two_value_arrays[j]){
+                    count += j-(i+1);
+                }
+            }
+        }
+        
+        return count;
+    }
 };
