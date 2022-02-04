ac47847dd521a4bfb5257c6632f7b3c2c309e1e7
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-02-04 23:31:34 +0900

    Add files via upload

---
+++ leetcode/midium/BFS DFS/419_Battleships in a Board.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        
+        int m = board.size();
+        int n = board[0].size();
+
+        int ans = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 'X'){
+                    ans++;
+                    dfs(board,i,j);
+                }
+            }
+        }
+
+        return ans;
+    }
+    
+    
+    void dfs(vector<vector<char>>& board, int i, int j){
+        
+        int m = board.size();
+        int n = board[0].size(); 
+        
+        if(i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '.') return;
+        
+        board[i][j] = '.';
+        
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
+        
+    }
+};
