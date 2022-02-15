205b16ea699b3ff01d554d48c55b4967f78abe7f
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-02-15 20:18:20 +0900

    Add files via upload

---
+++ leetcode/easy/BFS DFS/cpp/144_Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        
+        vector<int> ans;
+        dfs(root, ans);
+        
+        return ans;
+    }
+    
+    void dfs(TreeNode* root, vector<int>& ans){
+    
+        if(root){
+            
+            ans.push_back(root->val);
+            
+            if(root->left)  dfs(root->left, ans);
+            
+            if(root->right) dfs(root->right, ans);
+        
+        }
+        
+        return;
+    }
+};
