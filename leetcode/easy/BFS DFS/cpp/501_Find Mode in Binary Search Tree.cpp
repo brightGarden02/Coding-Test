3b5c124bee33c497278fa9b57167e1b679b5e03b
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-02-16 23:28:44 +0900

    Add files via upload

---
+++ leetcode/easy/BFS DFS/cpp/501_Find Mode in Binary Search Tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> modes;
+    int p = INT_MIN, cur, maxVal = INT_MIN;
+    
+    vector<int> findMode(TreeNode* root) {
+        
+        inorder(root);
+        return modes;
+    }
+    
+    
+    void inorder(TreeNode* root) {
+        
+        if(!root) return;
+        
+        if(root->left) inorder(root->left);
+        
+        if(p == root->val) cur++;
+        else cur = 1;
+        
+        if(cur == maxVal) {
+            modes.push_back(root->val);
+        }
+        
+        if(cur > maxVal) {
+            maxVal = cur;
+            modes.clear();
+            modes.push_back(root->val);
+        }
+        
+        p = root->val;
+        
+        if(root->right) inorder(root->right);
+    }
+};
