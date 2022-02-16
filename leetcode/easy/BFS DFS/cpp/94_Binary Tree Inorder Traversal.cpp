3ef16a4af454ac9165fe1a0dbf5ce47909ce3190
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-02-16 09:35:22 +0900

    Update 94_Binary Tree Inorder Traversal.cpp

--- leetcode/easy/BFS DFS/cpp/94_Binary Tree Inorder Traversal.cpp
+++ leetcode/easy/BFS DFS/cpp/94_Binary Tree Inorder Traversal.cpp
@@ -14,19 +14,20 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         
         vector<int> ans;
-        helper(root, ans);
+        inorder(root, ans);
         
         return ans;
     }
     
-    
-    void helper(TreeNode* root, vector<int>& ans){
+    void inorder(TreeNode* root, vector<int>& ans){
         
-        if(root == NULL) return;
+        if(root){
+            
+            inorder(root->left, ans);
+            ans.push_back(root->val);
+            inorder(root->right, ans);
+        }
         
-        helper(root->left, ans);
-        ans.push_back(root->val);
-        helper(root->right, ans);
-        
-    } 
+        return;
+    }
 };
