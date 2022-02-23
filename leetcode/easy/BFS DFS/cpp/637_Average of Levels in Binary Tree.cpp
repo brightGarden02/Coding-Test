399aee3a6eb820dcb7972a601f55ccb04082e6ac
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-02-24 00:01:18 +0900

    Add files via upload

---
+++ leetcode/easy/BFS DFS/cpp/637_Average of Levels in Binary Tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        
+        vector<double> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            
+            int n = q.size();
+            double sum = 0;
+            for(int i = 0; i < n; i++){
+                
+                TreeNode *curr = q.front();
+                q.pop();
+                sum += curr->val;
+                
+                if(curr->left != NULL){
+                    q.push(curr->left);    
+                }
+
+                if(curr->right != NULL){
+                    q.push(curr->right);    
+                }
+                   
+            }
+            
+            ans.push_back(sum/n);
+        }
+        
+     return ans;
+    }
+};
