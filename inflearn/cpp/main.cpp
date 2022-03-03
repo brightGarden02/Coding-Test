b720df793ee0a33066b5f0efc32531ee836f525e
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-03-03 12:14:10 +0900

    Add files via upload

---
+++ inflearn/cpp/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int digit_sum(int x);
+
+int main(){
+	
+	freopen("input.txt", "rt", stdin);
+
+	int n, num, i, sum, max = INT_MIN, res;
+	scanf("%d", &n);
+	
+	for(int i = 0; i < n; i++){
+		
+		scanf("%d", &num);
+		sum = digit_sum(num);
+		
+		if(sum > max){
+			max = sum;
+			res = num;		
+		}
+		else if(sum == max){
+			
+			if(num > res){
+				res = num;
+			}
+		}
+	}
+	cout << "res: " << res << endl;
+	
+	
+	return 0;
+}
+
+
+int digit_sum(int x){
+	
+	int sum = 0;
+	int rem = 0;
+	while(x > 0){
+		
+		rem = x % 10;
+		sum += rem;
+		x = x/10;
+	}
+	
+	
+	return sum;
+}
