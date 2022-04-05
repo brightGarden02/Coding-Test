75c4e90a285bdc32824003ae80a5fc2953911308
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-05 11:35:52 +0900

    Update 2864_5와6의차이_ing.java

--- baekJoon/greedy/2864_5와6의차이_ing.java
+++ baekJoon/greedy/2864_5와6의차이_ing.java
@@ -3,34 +3,75 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
     
-        // 1. n1, n2를 String으로 변환
-        // 2. String에서 5, 6인 것을 확인
-        // 3. 5를 확인했으면 ArrayList에 넣고
-        // 4. tmp에 5를 6으로 바꾸고 ArrayList에 넣기
-        // 5. 순서 3번, 4번을 6일 경우에도 반복
-        // 6. 또다른 ArrayList 만들어서 순서 3,4,5번 반복
-        // 7. for문 2개로 2개의 ArrayList 돌면서 최소값, 최대값 파악
-        
         Scanner sc = new Scanner(System.in);
-        
+    
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         
         String s1 = Integer.toString(n1);
-        ArrayList<Integer> al = new ArrayList<>();
-        al.add(n1);
+        String s2 = Integer.toString(n2);
+        
+        int minN1 = n1;
+        int maxN1 = n1;
+        
         for(int i = 0; i < s1.length(); i++){
+            
             if(s1.charAt(i) == '5'){
-               al.add 
-            }
-            else if(s1.charAt(i) == '6'){
                 
+               s1 = s1.substring(0, i) + '6' + s1.substring(i+1, s1.length());
+               int tmpN1 = Integer.parseInt(s1);
+               maxN1 = Math.max(maxN1, tmpN1);
             }
         }
         
+        for(int i = 0; i < s1.length(); i++){
+            
+            if(s1.charAt(i) == '6'){
+                
+            	s1 = s1.substring(0, i) + '5' + s1.substring(i+1, s1.length());
+                int tmpN1 = Integer.parseInt(s1);
+                minN1 = Math.min(minN1, tmpN1);
+            }
+        }
         
-        int answer = 0;
+        System.out.println("minN1: " + minN1 + ", maxN1: " + maxN1);
         
-        System.out.println(answer);
+        
+        int minN2 = n2;
+        int maxN2 = n2;
+        
+        for(int i = 0; i < s2.length(); i++) {
+        	
+        	if(s2.charAt(i) == '5'){      
+                
+                s2 = s2.substring(0, i) + '6' + s2.substring(i+1, s2.length());
+                int tmpN2 = Integer.parseInt(s2);
+                
+                maxN2 = Math.max(maxN2, tmpN2);
+             }
+        	
+        }
+        
+        for(int i = 0; i < s2.length(); i++) {
+        	
+        	if(s2.charAt(i) == '6'){
+                 
+            	 s2 = s2.substring(0, i) + '5' + s2.substring(i+1, s2.length());
+                 int tmpN2 = Integer.parseInt(s2);
+                 
+                 minN2 = Math.min(minN2, tmpN2);
+                 
+             }
+        	
+        }
+        
+       System.out.println("minN1: " + minN1 + ", maxN1: " + maxN1);
+       System.out.println("minN2: " + minN2 + ", maxN2: " + maxN2);
+       
+        
+       int minAnswer = minN1 + minN2;
+       int maxAnswer = maxN1 + maxN2;
+       System.out.println(minAnswer + " " + maxAnswer);
+	
 	}
 }
