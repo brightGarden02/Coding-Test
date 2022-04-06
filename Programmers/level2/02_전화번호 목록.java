27100b3f8217af4434519f980e92f90efc45f31a
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-06 19:05:45 +0900

    Update and rename level2/02_전화번호 목록.java to Programmers/level2/02_전화번호 목록.java

--- Programmers/level2/02_전화번호 목록.java
+++ Programmers/level2/02_전화번호 목록.java
@@ -2,8 +2,8 @@ import java.util.HashMap;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
         
+        boolean answer = true;
         HashMap<String, Integer> map = new HashMap<>();
         for(int i = 0; i < phone_book.length; i++){
             map.put(phone_book[i], i);
@@ -13,10 +13,8 @@ class Solution {
             
             for(int j = 1; j < phone_book[i].length(); j++){
                 if(map.containsKey(phone_book[i].substring(0, j))){
-                //    System.out.println("length:" + phone_book[i].length() + ", j: " + j + ", " + phone_book[i].substring(0, j));
                     return false;
-                    }
-                
+                }
             }
         }
         
