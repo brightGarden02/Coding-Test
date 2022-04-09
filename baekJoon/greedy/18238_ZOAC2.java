9646ca5d2769c314a1d4cde57d7a86a37d25036b
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-09 22:44:47 +0900

    Update and rename Main.java to 18238_ZOAC2.java

--- baekJoon/greedy/18238_ZOAC2.java
+++ baekJoon/greedy/18238_ZOAC2.java
@@ -1,5 +1,3 @@
-package coding_test0405;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 	// TODO Auto-generated method stub
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         
 
