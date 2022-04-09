c9f9169a9914e9e265fa250609ec0c9bbd7a4721
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-09 15:09:50 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,34 @@
+package coding_test0405;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        		
+        int sCount = 0;
+        int lCount = 0;
+        String seat = br.readLine();
+        
+        
+        for(int i = 0; i < n; i++){
+            if(seat.charAt(i) == 'S'){
+                sCount++;
+            }
+            else{
+                lCount++;
+            }
+        }
+        int ans = sCount + lCount/2 + 1;
+        ans = Math.min(ans, n);
+        System.out.println(ans);
+                
+	}
+
+}
