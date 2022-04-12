77209a3eba7d6d242bb36bf4d1942d0de2bd83a6
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-13 07:54:31 +0900

    Update and rename Main.java to 10610_30.java

--- baekJoon/greedy/10610_30.java
+++ baekJoon/greedy/10610_30.java
@@ -1,5 +1,3 @@
-package coding_test0405;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
         String s = br.readLine();
         int n = s.length();
