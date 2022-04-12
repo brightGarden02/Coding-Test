9e8373d9e62ce42e2de384c67378a6519deb099a
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-13 07:18:18 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,32 @@
+package coding_test0405;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+        long num = Long.parseLong(br.readLine());
+        long sum = 0;
+        int count = 0;
+        
+        int i = 1;
+        while(true){
+            
+            if(sum > num){
+                break;    
+            }
+            
+            sum += i;
+            i++;
+            count++;
+        }
+        
+		System.out.println(count-1);
+	}
+}
