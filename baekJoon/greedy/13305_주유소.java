5b81ca9688c70c48cb3a25cbdc944a40a134c32c
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-12 19:23:27 +0900

    Update and rename Main.java to 13305_주유소.java

--- baekJoon/greedy/13305_주유소.java
+++ baekJoon/greedy/13305_주유소.java
@@ -1,4 +1,3 @@
-package coding_test0405;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.io.BufferedReader;
@@ -11,14 +10,14 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st;
         
         int n = Integer.parseInt(br.readLine());
         
         st = new StringTokenizer(br.readLine());
 		
-		long[] sArr = new long[n-1];
+	long[] sArr = new long[n-1];
         for(int i = 0; i < n-1; i++) {
         	
         	sArr[i] = Long.parseLong(st.nextToken()); 
@@ -26,28 +25,28 @@ public class Main {
         
         long[] literArr = new long[n];
         st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
+	for(int i = 0; i < n; i++) {
 			
-			literArr[i] = Long.parseLong(st.nextToken());
-		}
+		literArr[i] = Long.parseLong(st.nextToken());
+	}
         
 		
         
-		long Ssum = 0;
-		for(int i = 0; i < n-1; i++) {
-			Ssum += sArr[i];
-		}
+	long Ssum = 0;
+	for(int i = 0; i < n-1; i++) {
+		Ssum += sArr[i];
+	}
 		
-		long answer = 0;
-		long literMin = literArr[0];
-		for(int i = 0; i < n - 1; i++) {
+	long answer = 0;
+	long literMin = literArr[0];
+	for(int i = 0; i < n - 1; i++) {
 			
-			if(literArr[i] < literMin) {
-				literMin = literArr[i];
-			}
-			
-			answer += (literMin * sArr[i]);
+		if(literArr[i] < literMin) {
+			literMin = literArr[i];
 		}
+			
+		answer += (literMin * sArr[i]);
+	}
 		
         System.out.println(answer);
 	}
