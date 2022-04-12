321f49151fb3cf60cfee45a40acc79de76ba1baf
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-12 11:02:44 +0900

    Update and rename Main.java to 2217_로프.java

--- baekJoon/greedy/2217_로프.java
+++ baekJoon/greedy/2217_로프.java
@@ -1,23 +1,11 @@
-package coding_test0405;
-
 import java.util.Arrays;
-/*
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-*/
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-//		2
-//		10
-//		15
 		int n = sc.nextInt();
 		int[] lopes = new int[n];
 		
