3ea9f92b12936bcfd14da94bd52ef18a2e4048da
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-14 10:20:17 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,50 @@
+package coding_test0405;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+       
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		String sB = "";
+		int ans = 1;
+		
+		while(A != B) {
+			
+			if(B % 2 == 0) {
+				
+				B = B / 2;
+			}
+			else {
+				
+				if(B % 10 == 1) {
+					B = B / 10;
+				}
+				else {
+					System.out.println(-1);
+					return;
+				}
+			}
+			
+			ans++;
+			if(A > B) {
+				System.out.println(-1);
+				return;
+			}
+			
+			System.out.println("A : " + A + ", B: " + B);
+		}
+		
+		System.out.println(ans);
+	}
+}
