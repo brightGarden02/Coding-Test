eea2a6cca0dbd8b899803690254c7fbf6ec1ac6f
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-16 09:58:08 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,43 @@
+package coding_test0405;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int[] brandArr = new int[M];
+		int[][] costArr = new int[M][2];
+		
+		for(int i = 0; i < M; i++) {
+			StringTokenizer cost = new StringTokenizer(br.readLine());
+			costArr[i][0] = Integer.parseInt(cost.nextToken());
+			costArr[i][1] = Integer.parseInt(cost.nextToken());
+			
+		}
+		
+		int minPackageCost = Integer.MAX_VALUE;
+		int minOneCost = Integer.MAX_VALUE;
+		for(int i = 0; i < M; i++) {
+			
+			minPackageCost =  Math.min(minPackageCost, costArr[i][0]);
+			minOneCost =  Math.min(minOneCost, costArr[i][1]);
+			
+		}
+		
+		int ans = 0;
+		ans = Math.min(((N/6)+1) * minPackageCost, N * minOneCost);
+		ans = Math.min(ans, (N/6) * minPackageCost + (N%6) * minOneCost);
+		
+		
+		System.out.println("ans: " + ans);
+	}
+}
