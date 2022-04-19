5505b4e4354c7e6d3ebd6a9c4fb6ee14c8ff3eb5
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-19 13:33:15 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,32 @@
+package coding_test0405;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int n = str.length();
+		
+		String makeUCPC = "UCPC";
+		int j = 0;
+		for(int i = 0; i < n; i++) {
+			
+			if(str.charAt(i) == makeUCPC.charAt(j)) {
+				j++;
+			}
+			if(j == 4) {
+				System.out.println("I love UCPC");
+				return;
+			}	
+		}
+		
+		System.out.println("I hate UCPC");
+		
+			
+	}
+}
