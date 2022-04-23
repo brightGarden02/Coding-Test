422fe0855b6500e67adf7e7a5d993845c9e80794
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-24 08:57:46 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,38 @@
+package coding_test0405;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main{
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int ans = 0;
+		Integer[] costArr= new Integer[n];
+		
+		for(int i = 0; i < n; i++) {
+			costArr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(costArr, Comparator.reverseOrder());
+		for(int i = 0; i < n; i++) {
+			
+			if(i % 3 == 2) {
+				continue;
+			}
+			else {
+				ans += costArr[i];
+			}
+		}
+		
+		
+		System.out.println(ans);
+		
+	}
+}
