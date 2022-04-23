05a2c25795cf1bbb92478996de5e9a6c67176f4a
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-23 10:47:50 +0900

    Update and rename Main.java to 2828_사과 담기 게임.java

--- baekJoon/greedy/2828_사과 담기 게임.java
+++ baekJoon/greedy/2828_사과 담기 게임.java
@@ -1,5 +1,3 @@
-package coding_test0405;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,8 +8,6 @@ public class Main{
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 			
 		int screenSize = Integer.parseInt(st.nextToken());
@@ -21,15 +17,11 @@ public class Main{
 		
 		
 		int apples = Integer.parseInt(br.readLine());
-		
 		int[] applePosArr = new int[apples+1];
 		
 		for(int i = 1; i <= apples; i++) {
 			applePosArr[i] = Integer.parseInt(br.readLine());
 		}
-		
-		
-		
 		
 		int ans = 0;
 		for(int i = 1; i <= apples; i++) {
@@ -53,8 +45,6 @@ public class Main{
 					bright = bleft + bucketSize - 1; 
 				}
 			}
-			System.out.println("bleft: " + bleft + ", bright: " + bright);
-			
 		}
 		System.out.println(ans);
 		
