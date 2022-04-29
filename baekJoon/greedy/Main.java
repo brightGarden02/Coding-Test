e1af67030883584e5017c49a2b3d7df919c645fd
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-29 11:08:14 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,35 @@
+package codingMain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i++){
+            st.push(Integer.parseInt(br.readLine()));
+        }
+
+        while(!st.isEmpty()){
+
+            int tmp = st.pop();
+            if(tmp == n){
+                n--;
+            }
+            else{
+                ans++;
+            }
+        }
+
+
+        System.out.println(ans);
+    }
+}
