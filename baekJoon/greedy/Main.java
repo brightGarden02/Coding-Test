38705e47aa4714fc951752e0b1605fc66379d72c
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-29 10:10:00 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,30 @@
+package codingMain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double ans = 0;
+        Integer[] arr = new Integer[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        ans += arr[0];
+        for(int i = 1; i < n; i++){
+            ans += arr[i]/2.0;
+        }
+
+        System.out.println(ans);
+
+    }
+}
