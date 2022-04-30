f09055daddfc7a62632dd4ab122377ebdb824d7a
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-04-30 13:42:58 +0900

    Update Main.java

--- leetcode/easy/greedy/Main.java
+++ leetcode/easy/greedy/Main.java
@@ -1,62 +1,37 @@
-package codingMain;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-
-        int rTmpCount = 0;
-        int rCount = 0;
-
-        int bTmpCount = 0;
-        int bCount = 0;
-        for(int i = 0; i < n; i++){
-            if(s.charAt(i) == 'R'){
-                rTmpCount++;
-
-                bCount += bTmpCount;
-                bTmpCount = 0;
+class Solution {
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        
+        int n = nums.length;
+        int ans = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < n && k > 0; i++){
+            
+            if(nums[i] < 0){
+                nums[i] = -nums[i];
+                k--;
             }
+            else if(nums[i] == 0){
+                k = 0;
+            }
+        }    
+        Arrays.sort(nums);
+        for(int i = 0; i < n && k > 0; i++){
+            
+            if(k % 2 == 0){
+                k = 0;    
+            } 
             else{
-                bTmpCount++;
-
-                rCount += rTmpCount;
-                rTmpCount = 0;
+                nums[i] = -nums[i];
             }
+            k--;
         }
-
-        int answer1 = Math.min(rCount, bCount);
-
-
-        rTmpCount = 0;
-        rCount = 0;
-
-        bTmpCount = 0;
-        bCount = 0;
-        for(int i = n-1; i >= 0; i--){
-            if(s.charAt(i) == 'R'){
-                rTmpCount++;
-
-                bCount += bTmpCount;
-                bTmpCount = 0;
-            }
-            else{
-                bTmpCount++;
-
-                rCount += rTmpCount;
-                rTmpCount = 0;
-            }
+        
+        
+        for(int num : nums){
+            ans += num;
         }
-
-        int answer2 = Math.min(rCount, bCount);
-        int answer = Math.min(answer1, answer2);
-        System.out.println(answer);
+        
+        
+        return ans;
     }
 }
