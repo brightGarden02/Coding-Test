9d1e6267ed30ce4dea7657e25f01d83cb5eafab3
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-05-01 11:19:12 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,56 @@
+package codingMain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        long ans = 0;
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer stData = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+
+                arr[i][j]= Integer.parseInt(stData.nextToken());
+                ans += arr[i][j];
+            }
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            int maxVal = Integer.MIN_VALUE;
+            for(int j = 0; j < m; j++){
+
+                maxVal = Math.max(maxVal, arr[i][j]);
+            }
+            set.add(maxVal);
+        }
+
+        for(int i = 0; i < m; i++){
+            int maxVal = Integer.MIN_VALUE;
+            for(int j = 0; j < n; j++){
+
+                maxVal = Math.max(maxVal, arr[j][i]);
+            }
+            set.add(maxVal);
+        }
+
+        Iterator<Integer> iter= set.iterator();
+        while(iter.hasNext()){
+            ans -= iter.next();
+        }
+
+
+        System.out.println(ans);
+    }
+}
