947f5fdf47c5922cf44b2862ca7305941b0ef061
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-05-02 13:13:57 +0900

    Add files via upload

--- leetcode/easy/greedy/Main.java
+++ leetcode/easy/greedy/Main.java
@@ -1,37 +1,38 @@
-class Solution {
-    public int largestSumAfterKNegations(int[] nums, int k) {
-        
-        int n = nums.length;
-        int ans = 0;
-        Arrays.sort(nums);
-        for(int i = 0; i < n && k > 0; i++){
-            
-            if(nums[i] < 0){
-                nums[i] = -nums[i];
-                k--;
-            }
-            else if(nums[i] == 0){
-                k = 0;
-            }
-        }    
-        Arrays.sort(nums);
-        for(int i = 0; i < n && k > 0; i++){
-            
-            if(k % 2 == 0){
-                k = 0;    
-            } 
-            else{
-                nums[i] = -nums[i];
-            }
-            k--;
+package codingMain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int couldUse = 0;
+        int campingPeroid = 0;
+        int vacation = 0;
+        int i = 1;
+        while(true){
+
+            String s = br.readLine();
+            String[] arr = s.split(" ");
+
+            couldUse = Integer.parseInt(arr[0]);
+            if(couldUse == 0) return;
+
+            campingPeroid = Integer.parseInt(arr[1]);
+            vacation = Integer.parseInt(arr[2]);
+
+
+            int div = vacation / campingPeroid;
+            int rem = vacation % campingPeroid;
+            int ans = (div * couldUse) + Math.min(rem, couldUse);
+
+            System.out.println("Case " + i + ": " + ans);
+            i++;
         }
-        
-        
-        for(int num : nums){
-            ans += num;
-        }
-        
-        
-        return ans;
+
     }
 }
