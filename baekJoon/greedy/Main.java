d7c4056ed43b87a1bca84359f8e500166eb994c2
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-05-05 06:58:51 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,37 @@
+package codingMain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        StringTokenizer students = new StringTokenizer(br.readLine());
+        int[] arr1 = new int[n];
+        for(int i = 0; i < n; i++){
+            arr1[i] = Integer.parseInt(students.nextToken());
+        }
+        int[] diffArr = new int[n-1];
+        for(int j = 0; j < n-1; j++){
+            diffArr[j] = arr1[j+1] - arr1[j];
+        }
+
+        Arrays.sort(diffArr);
+        int ans = 0;
+        for(int j = 0; j < n-k; j++){
+            ans += diffArr[j];
+        }
+
+
+        System.out.println(ans);
+    }
+}
