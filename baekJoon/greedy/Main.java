80b24c71eafa87fb40b15cb71db339a07a636a3e
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-05-06 16:00:56 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,37 @@
+package codingMain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuffer S = new StringBuffer(br.readLine());
+        StringBuffer T = new StringBuffer(br.readLine());
+
+
+        while(S.length() < T.length()){
+
+            if(T.charAt(T.length()-1) == 'A'){
+                T.deleteCharAt(T.length()-1);
+            }
+            else{
+                T.deleteCharAt(T.length()-1);
+                T.reverse();
+            }
+        }
+
+        if(S.toString().equals(T.toString())){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+
+    }
+}
