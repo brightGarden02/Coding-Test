5177a959bad0a6cc4d9b6165d4b9bcdbcafc3755
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-05-11 15:19:07 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,53 @@
+package codingMain;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]){
+                    return o1[1] - o2[1];
+                }
+                else{
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(arr[0][1]);
+
+        int answer = 1;
+        for(int i = 1; i < arr.length; i++){
+
+            while(!pq.isEmpty() && pq.peek() <= arr[i][0]){
+                pq.poll();
+            }
+            pq.add(arr[i][1]);
+            answer = Math.max(answer, pq.size());
+
+        }
+
+
+        System.out.println(answer);
+    }
+}
