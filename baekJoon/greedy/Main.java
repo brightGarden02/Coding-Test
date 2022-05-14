f1b6d43ff70825d99b8597e7475c41898e912618
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-05-14 12:43:50 +0900

    Add files via upload

---
+++ baekJoon/greedy/Main.java
@@ -0,0 +1,59 @@
+package codingMain;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> crainList = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            crainList.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(crainList, Collections.reverseOrder());
+
+        int m = Integer.parseInt(br.readLine());
+        List<Integer> boxList = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < m; i++){
+            boxList.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(boxList, Collections.reverseOrder());
+
+
+        int answer = 0;
+        if(crainList.get(0) < boxList.get(0)) System.out.println(-1);
+        else{
+
+            while(!boxList.isEmpty()){
+                int boxIdx = 0;
+                int crainIdx = 0;
+                while(crainIdx < crainList.size()){
+
+                    if(boxIdx == boxList.size()){
+                        break;
+                    }
+                    else if(crainList.get(crainIdx) >= boxList.get(boxIdx)){
+                        boxList.remove(boxIdx);
+                        crainIdx++;
+                    }
+                    else {
+                        boxIdx++;
+                    }
+
+                }
+                answer++;
+            }
+
+            System.out.println(answer);
+        }
+
+    }
+}
