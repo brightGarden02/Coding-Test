5fcde4cd2e5ce64a1f31c3c6177cb820a01551b5
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-07-06 11:14:00 +0900

    Update 1012_유기농 배추.java

--- baekJoon/BFS/1012_유기농 배추.java
+++ baekJoon/BFS/1012_유기농 배추.java
@@ -1,60 +1,53 @@
-package ex;
+package coding;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-
     private static int m, n, k;
-    private static int count;
-
-    private static int[][] area;
-    private static boolean[][] visited;
+    private static int[][] arr;
     private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, -1, 1};
-
+    private static int[] dy = {0, 0, 1, -1};
+    private static boolean[][] visited;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        for(int a = 0; a < T; a++){
+        while(T-- > 0){
 
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
 
-            area = new int[m][n];
-            visited = new boolean[m][n];
+            arr = new int[n][m];
+            visited = new boolean[n][m];
 
-            while(k > 0){
+            while(k-- > 0){
 
                 st = new StringTokenizer(br.readLine());
-                int y = Integer.parseInt(st.nextToken());
                 int x = Integer.parseInt(st.nextToken());
-
-                area[x][y] = 1;
-
-                k--;
+                int y = Integer.parseInt(st.nextToken());
+                arr[y][x] = 1;
             }
 
-            count = 0;
-            for(int i = 0; i < m; i++){
-
-                for(int j = 0; j < n; j++){
-                    if(area[i][j] == 1 && !visited[i][j]){
-                        bfs(i, j);
+            int count = 0;
+            for (int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    if(arr[i][j] == 1 && !visited[i][j]){
                         count++;
+                        bfs(i, j);
                     }
                 }
             }
 
             System.out.println(count);
         }
-
     }
 
     private static void bfs(int x, int y) {
@@ -64,25 +57,24 @@ public class Main {
         visited[x][y] = true;
 
         while(!queue.isEmpty()){
-            int[] now = queue.poll();
 
+            int[] now = queue.poll();
             int nowX = now[0];
             int nowY = now[1];
 
             for(int i = 0; i < 4; i++){
-
                 int nextX = nowX + dx[i];
                 int nextY = nowY + dy[i];
 
-                if(nextX >= 0 && nextY >= 0 && nextX < m && nextY < n){
+                if(nextX >= 0 && nextY >= 0 && nextX < n && nextY < m
+                && !visited[nextX][nextY] && arr[nextX][nextY] == 1){
 
-                    if(area[nextX][nextY] == 1 && !visited[nextX][nextY]){
-                        visited[nextX][nextY] = true;
-                        queue.add(new int[] {nextX, nextY});
-                    }
+                    visited[nextX][nextY] = true;
+                    queue.add(new int[] {nextX, nextY});
                 }
             }
         }
+
 
     }
 }
