7361830fb790c1f5af4e202910fe66c86f870dbb
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-07-08 11:02:20 +0900

    Add files via upload

---
+++ baekJoon/Implementation/Main.java
@@ -0,0 +1,65 @@
+package coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        int answer = 0;
+        while(T-- > 0){
+
+            String str = br.readLine();
+            boolean flag = true;
+            for(int i = 0; i < str.length()-2 && flag; i++){
+                char c1 = str.charAt(i);
+
+                for(int j = i+1; j < str.length()-1 && flag; j++){
+                    char c2 = str.charAt(j);
+
+
+                    if(j-i == 1 && c1 == c2){
+                        int k = j+1;
+                        char c3 = str.charAt(k);
+                        while(c1 == c3 && k < str.length()){
+                            c3 = str.charAt(k);
+                            k++;
+                        }
+
+                        if(k < str.length()){
+                            for( ; k < str.length() && flag; k++){
+                                c3 = str.charAt(k);
+                                if(c1 == c3){
+                                    flag = false;
+                                }
+                            }
+                        }
+                    }
+
+
+                    if(c1 != c2){
+                        for(int k = j+1; k < str.length() && flag; k++){
+                            char c3 = str.charAt(k);
+                            if(c1 == c3){
+                                flag = false;
+
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            if(flag){
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
