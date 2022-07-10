4de47a98d68ddaa1202fd3f57afc5fd44d359e58
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-07-11 08:35:44 +0900

    Add files via upload

---
+++ baekJoon/Sorting/Main.java
@@ -0,0 +1,50 @@
+package coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int midX = arr[n/2][0];
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        int midY = arr[n/2][1];
+
+        long answer = 0;
+        for(int i = 0; i < n; i++){
+            answer += Math.abs(midX - arr[i][0]) + Math.abs(midY - arr[i][1]);
+        }
+
+        System.out.println(answer);
+    }
+}
