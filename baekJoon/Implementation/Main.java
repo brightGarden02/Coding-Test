f0055af816a7fa241a0b81d5a21a6e674ebf3a1d
JuWon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-08-06 22:05:45 +0900

    Add files via upload

---
+++ baekJoon/Implementation/Main.java
@@ -0,0 +1,19 @@
+package coding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        System.out.println(s.length());
+
+
+    }
+}
